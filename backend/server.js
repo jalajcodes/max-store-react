44e89730792ede5762d7d742adb6c969f0d1da11
jalajcodes <jalaj@mail.com>
2022-03-23 20:36:27 +0530

    Fix

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,7 @@ import colors from "colors";
 dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+app.use(express.json());
 
 app.get("/api/products", (req, res) => {
   res.json(products);
@@ -21,17 +19,17 @@ app.get("/api/products/:id", (req, res) => {
 
 const __dirname = path.resolve();
 
-// if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, "/frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-);
-// } else {
-// app.get("/", (req, res) => {
-// res.send("API is running....");
-// });
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 const PORT = process.env.PORT || 5001;
 
