a298b971abd13d8b6722b21dd309ca11e50d78d2
jalajcodes <jalaj@mail.com>
2022-03-23 20:34:56 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -21,17 +21,17 @@ app.get("/api/products/:id", (req, res) => {
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+);
+// } else {
+// app.get("/", (req, res) => {
+// res.send("API is running....");
+// });
+// }
 
 const PORT = process.env.PORT || 5001;
 
