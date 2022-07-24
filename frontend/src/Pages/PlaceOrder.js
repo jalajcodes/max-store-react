8bc681445d82f1bef2acf653d90a984b5564c249
jalajcodes <jalaj@mail.com>
2022-07-24 20:17:37 +0530

    refactor

--- frontend/src/Pages/PlaceOrder.js
+++ frontend/src/Pages/PlaceOrder.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../Contexts/authContext";
 import { useCart } from "../Contexts/cartContext";
